8cd013a19b1553ef149ea6c1f72c16d3d5824bf1
Oleksandr <alexandrenko3006@gmail.com>
2025-07-21 18:41:06 +0300

    Setting dockers files

--- back-end/src/knexfile.ts
+++ back-end/src/knexfile.ts
@@ -1,8 +1,9 @@
 import {Knex} from "knex"
 
 import dotenv from "dotenv"
+import path from "path"
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const config: { [key: string]: Knex.Config } = {
   development: {
